041153fc40532be6ff2360c76278c18c800e08ca
Samer Saied <samerpasha@hotmail.com>
2021-10-08 03:17:17 +0200

    change admin panel name - change Logo

--- config/server.js
+++ config/server.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   admin: {
+    url: '/adminPanel',
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'f4dfd445be0646a9c5cae72125b3c32d'),
     },
